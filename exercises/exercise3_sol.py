fc264494f3fbbc55f60ca0256e4ce48788bb5c7f
annasun19 <42841171+annasun19@users.noreply.github.com>
2021-02-09 20:01:16 -0500

    Update exercise3_sol.py

--- exercises/exercise3_sol.py
+++ exercises/exercise3_sol.py
@@ -1,14 +1,14 @@
 # Exercise 3a Solution
-age = 2019 - 2001
-for i in range(2019, 2030):
+age = 2021 - 2001
+for i in range(2021, 2032):
     print("In ", i, ", I am ", age, " years old")
     age += 1
 
 
 # Exercise 3b Solution
-age = 2019 - 2001
-i = 2019
-while i < 2030:
+age = 2021 - 2001
+i = 2021
+while i < 2032:
     print("In ", i, ", I am ", age, " years old")
     age += 1
     i += 1
